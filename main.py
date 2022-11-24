86fb89aa63e845d9ef9d172c3ed184394c3ac2a1
taylancode <88141669+taylancode@users.noreply.github.com>
2022-11-24 17:28:59 +0000

    added API endpoints

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-from lib.logic import wiki
+import uvicorn
+from fastapi import FastAPI
+from lib.logic import wiki, wiki_search
 
-print(wiki())
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Wiki API"}
+
+
+@app.get("/search/{name}")
+async def search(name: str) -> str:
+    return {"result": wiki_search(name)}
+
+
+@app.get("/wiki/{name}")
+async def page(name: str) -> str:
+    return {"result": wiki(name)}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
